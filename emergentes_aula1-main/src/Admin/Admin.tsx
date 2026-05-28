72eb78f144030845d93b752ef9a3ac03336df1f3
Anthony Martins de Castro <anthonymartins1997@gmail.com>
2026-05-28 16:55:03 -0300

    updt rota de resumo adm

--- emergentes_aula1-main/src/Admin/Admin.tsx
+++ emergentes_aula1-main/src/Admin/Admin.tsx
@@ -8,7 +8,7 @@ export default function Admin() {
   useEffect(() => {
     async function fetchDados() {
       try {
-        const res = await axios.get("https://emergentes-aula1-main-vb57.vercel.app/animais/animais/resumo");
+        const res = await axios.get("https://emergentes-aula1-main-6evg.vercel.app/animais/animais/resumo");
         setDados(res.data);
       } catch (err) {
         console.error("Erro ao buscar dados do gráfico:", err);
